f5c8c2ff2764a1804463ca68c804425a79ff1f34
NickDorkins <nick.dorkins@gmail.com>
2021-02-07 17:50:17 -0800

    decrypt complete

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -7,15 +7,16 @@ def encrypt(string, shift):
     message = "" 
 
     # Clean the shift number before running it through the code block
-    # Account for 0 shifts and negative shifts
-    if shift <= 0:
+    # Account for 0 shifts and negative inputs
+    key = shift % 26
+    if key == 0:
         return('Invalid shift key')
-    if shift >= 26:
-        key = shift % 26
+    
+    if key < 0:
+        key = key + 26
         if key == 0:
             return('Invalid shift key')
-    else:
-        key = shift
+   
     
     print('key:', key)
     # # Establish length of input string
@@ -26,31 +27,33 @@ def encrypt(string, shift):
         
         # Convert letter to ASCII numerical value
         numeric = ord(string[letter])
-        print('numeric', numeric)
+        # print('numeric', numeric)
 
         # Account for spaces
         if numeric == 32:
-            print('numeric before encrypted range statement', numeric)
             encrypted = numeric
-            print('encrypted range statement', encrypted)
+            # print('encrypted == 32 statement', encrypted)
         else:    
             # Add the input shift amount to the ASCII value
             encrypted = numeric + key
-            print('encrypted + key', encrypted)
+            # print('encrypted + key', encrypted)
 #    Check values to ensure that they are within the range that we would like (65-90:Uppercase and 97-122:Lowercase)
       
-        if encrypted < 65:
-            encrypted + 26
-            # print('Under65:', encrypted)
-        elif encrypted > 90:
-            encrypted - 26
-            # print(encrypted)
-        elif encrypted < 97 :
-            encrypted + 26
-            # print(encrypted)
-        elif encrypted > 122: 
-            encrypted - 26
-            # print(encrypted)
+        if numeric >= 65 and numeric <= 90:
+            if encrypted < 65: 
+                encrypted = encrypted + 26
+                print('Under65:', encrypted)
+            elif encrypted > 90:
+                encrypted = encrypted - 26
+                print('Over90', encrypted)
+
+        if numeric >= 97 and numeric <= 122:
+            if encrypted < 97:
+                encrypted = encrypted + 26
+                print('Under97', encrypted)
+            elif encrypted > 122: 
+                encrypted = encrypted - 26
+                print('Over122', encrypted)
             
             # if (encrypted > 65 and encrypted < 90) or (encrypted > 97 and encrypted < 122): 
         message = message+chr(encrypted)
@@ -59,14 +62,22 @@ def encrypt(string, shift):
 
 # print(encrypt('hello', 1))
 # print(encrypt('JeLlO', 27))
-# print(encrypt('ALL CAPITAL LETTERS', 1))
-# print(encrypt('all lowercase letters', 1))
-print(encrypt('It was the best of times, it was the worst of times.', 1))
+# print(encrypt('ALL CAPITAL LETTERS ZZZ', -100))
+# print(encrypt('all lowercase letters zzz', -100))
+# print(encrypt('It was the best of times, it was the worst of times.', 1))
     
 # Create a decrypt function that takes in encrypted text and numeric shift which will restore the encrypted text back to its original form when correct key is supplied.
 
+def decrypt(text, key):
+    readable_text = encrypt(text, -key)
+    return readable_text
+
+print('Decrypt')    
+print(decrypt('EPP GETMXEP PIXXIVW DDD', 4))
 
 # create a crack function that will decode the cipher so that an encrypted message can be transformed into its original state WITHOUT access to the key.
+
+
 
 
 
